bf370d07ddced10513d1d7d0ceff302fc720bd18
Karlijnvh <112547700+Karlijnvh@users.noreply.github.com>
2024-02-26 15:23:20 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
+let toestand = 1
 basic.forever(function () {
-    basic.showNumber(Math.floor(input.lightLevel() / 26))
+    if (toestand == 1) {
+        if (input.lightLevel() < 100) {
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                `)
+            toestand = 2
+        }
+    }
+    if (input.lightLevel() >= 100) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        toestand = 1
+    }
 })
